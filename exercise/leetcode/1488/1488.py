1a69d54fff87b58189595ba9857b6d8abfbd6e79
zhuhonglinX <zhuhl1996@gmail.com>
2020-09-24 17:20:24 +0800

    update

---
+++ exercise/leetcode/1488/1488.py
@@ -0,0 +1,15 @@
+from typing import List
+
+class Solution:
+    def avoidFlood(self, rains: List[int]) -> List[int]:
+        left_set, right_set = set(), set()
+        pc = 0
+        record = []
+        pos0 = []
+
+        
+
+
+    
+
+    
