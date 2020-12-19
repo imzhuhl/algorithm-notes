d4eb7811ea1be74e9585424f46cf1c2569a4d74c
zhuhonglinX <zhuhl1996@gmail.com>
2020-12-19 23:11:38 +0800

    update

--- exercise/leetcode/0416/0416.py
+++ exercise/leetcode/0416/0416.py
@@ -1,9 +1,3 @@
-'''
-Author       : Zhu Honglin
-Date         : 2020-09-14 17:14:18
-LastEditTime : 2020-09-14 19:13:23
-'''
-
 from typing import List
 
 class Solution:
@@ -30,6 +24,45 @@ class Solution:
         return False
 
 
+class Solution2:
+    """动态规划
+    """
+    def canPartition(self, nums: List[int]) -> bool:
+        s = sum(nums)
+        if s % 2 != 0:
+            return False
+        target = s // 2
+        dp = [[False for _ in range(target+1)] for _ in range(len(nums) + 1)]
+        for i in range(len(nums)+1):
+            dp[i][0] = True
+        for i in range(1, len(nums)+1):
+            for j in range(1, target+1):
+                if j - nums[i-1] < 0:
+                    dp[i][j] = dp[i-1][j]
+                else:
+                    dp[i][j] = dp[i-1][j] or dp[i-1][j-nums[i-1]]
+
+        return dp[-1][-1]
+
+
+class Solution3:
+    """动态规划 状态压缩
+    """
+    def canPartition(self, nums: List[int]) -> bool:
+        s = sum(nums)
+        if s % 2 != 0:
+            return False
+        target = s // 2
+        dp = [False for _ in range(target+1)]
+        dp[0] = True
+        for i in range(1, len(nums)+1):
+            for j in range(target, 0, -1):
+                if j - nums[i-1] >= 0:
+                    dp[j] = dp[j] or dp[j-nums[i-1]]
+
+        return dp[-1]         
+
+
 if __name__ == '__main__':
-    sl = Solution()
-    print(sl.canPartition([1, 2, 5]))
+    sl = Solution3()
+    print(sl.canPartition([1, 5, 11, 5]))
